46b12628a0ee1c7bee575c2b3c30a9c0ce5e08a1
Hítalo Silva <hitalos@gmail.com>
2015-08-06 23:23:13 -0300

    Adicionada Lotação no campo de notas do vCard

--- views/vcard.php
+++ views/vcard.php
@@ -20,6 +20,7 @@ $vcard = new VCard();
 $vcard->addName($lastName, $firstName, $middleNames, '', '');
 $vcard->addJobtitle($result[0]['title'][0]);
 $vcard->addCompany('Justiça Federal em Alagoas');
+$vcard->addNote('Lotação: ' . utf8_encode($result[0]['department'][0] . ' - ' . $result[0]['physicaldeliveryofficename'][0]));
 $vcard->addEmail($result[0]['mail'][0], 'WORK');
 $vcard->addPhoneNumber('082 2122-' . $result[0]['ipphone'][0], 'PREF;WORK');
 if(isset($result[0]['mobile'][0])){
