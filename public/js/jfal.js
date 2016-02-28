728c9f397d2c2a70d95af356cbcdc1f6a658ddcf
Hítalo Silva <hitalos@gmail.com>
2016-02-28 00:39:35 -0300

    Criação de templates e diretivas no angular

--- public/js/jfal.js
+++ public/js/jfal.js
@@ -1,9 +1,20 @@
-var bina = angular.module('bina', []);
+angular.module('bina', [])
+    .controller('buscaController', function($scope, $http){
+        $http.get('/contatos/json').success(function(data){
+            $scope.contatos = data;
+        });
 
-bina.controller('buscaController', function($scope, $http){
-    $http.get('/contatos/json').success(function(data){
-        $scope.contatos = data;
+        $('#search').focus();
+    })
+    .directive('cardsCounter', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/cards-counter.html'
+        };
+    })
+    .directive('card', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/card.html'
+        };
     });
-
-    $('#search').focus();
-});
