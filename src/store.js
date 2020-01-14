08b8047a48762a899c8a5f1f27982bbbe911ccc5
Hítalo Silva <hitalos@gmail.com>
2020-01-14 18:18:16 -0300

    Fix suggestion from JS linter

--- src/store.js
+++ src/store.js
@@ -33,41 +33,39 @@ const show = (contact, searchTerms) => {
   return terms.some((str) => phones.some((phone) => phone.indexOf(str) >= 0))
 }
 /* eslint no-new: 0 */
-export default (Vuex) => {
-  return new Vuex.Store({
-    state: {
-      contacts: [],
-      searchTerms: ''
+export default (Vuex) => new Vuex.Store({
+  state: {
+    contacts: [],
+    searchTerms: ''
+  },
+  getters: {
+    total(state) {
+      return state.contacts.length
     },
-    getters: {
-      total(state) {
-        return state.contacts.length
-      },
-      count(state) {
-        return state.contacts.filter((contact) => contact.show).length
-      },
-      limitedList(state) {
-        return state.contacts
-          .filter((contact) => contact.show)
-          .slice(0, 30)
-      },
+    count(state) {
+      return state.contacts.filter((contact) => contact.show).length
     },
-    mutations: {
-      /* eslint no-param-reassign: 0, no-alert: 0 */
-      populate(state) {
-        axios.get('/contacts/all.json').then((response) => {
-          state.contacts = response.data.map((contact) => Object.assign(contact, { show: false }))
-        }).catch((err) => {
-          if (err.response.data.name === 'InvalidCredentialsError') {
-            alert('Erro!\nCredenciais do usuário de sistema inválidas.\nContate um administrador do domínio!')
-          }
-          console.log(err.response.data)
-        })
-      },
-      filterChanged(state, searchTerms) {
-        state.searchTerms = searchTerms
-        state.contacts = state.contacts.map((contact) => Object.assign(contact, { show: show(contact, searchTerms) }))
-      },
+    limitedList(state) {
+      return state.contacts
+        .filter((contact) => contact.show)
+        .slice(0, 30)
     },
-  })
-}
+  },
+  mutations: {
+    /* eslint no-param-reassign: 0, no-alert: 0 */
+    populate(state) {
+      axios.get('/contacts/all.json').then((response) => {
+        state.contacts = response.data.map((contact) => Object.assign(contact, { show: false }))
+      }).catch((err) => {
+        if (err.response.data.name === 'InvalidCredentialsError') {
+          alert('Erro!\nCredenciais do usuário de sistema inválidas.\nContate um administrador do domínio!')
+        }
+        console.log(err.response.data)
+      })
+    },
+    filterChanged(state, searchTerms) {
+      state.searchTerms = searchTerms
+      state.contacts = state.contacts.map((contact) => Object.assign(contact, { show: show(contact, searchTerms) }))
+    },
+  },
+})
